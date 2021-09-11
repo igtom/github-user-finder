1d74ac56f3cb16291e1c2b32aabf22d87cdf9dee
igtom <igtom.martin@gmail.com>
2021-09-12 00:06:53 +0200

    functionality

--- src/app/services/user-finder.service.ts
+++ src/app/services/user-finder.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserFinderService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  URL: string = 'https://api.github.com/users';
+
+  getUser(name): Observable<any> {
+    return this.http.get(`${this.URL}/${name}`);
+  }
 }
